9f39293243ddd3322f3efe59ed084a696df5de48
Abhisheknayak-programer <sahiservice300@gmail.com>
2024-07-13 13:02:59 +0530

    session 24 done

---
+++ Day 26/script.js
@@ -0,0 +1,37 @@
+console.log("Abhishek Nayak");
+console.log(66 * 55 * 34);
+console.log((234 + 333 - 33) / 3);
+
+let x;
+x = 454 + 6;
+console.log(x);
+x = 50;
+console.log(x);
+
+const y = 345.4567;
+console.log(y);
+
+var t = 20;
+console.log(t);
+t = 2345;
+console.log(t);
+
+// Math Operations
+console.log(Math.PI);
+console.log(Math.pow(2, 4));
+console.log(Math.sqrt(25));
+console.log(Number("345"));
+console.log(typeof "345");
+console.log(typeof 345);
+console.log(Math.floor(354.8));
+console.log(Math.ceil(354.8));
+console.log(Math.round(54.73));
+console.log(Math.trunc(235.90965));
+console.log(Math.abs(-4560));
+console.log(Math.abs(4560));
+console.log(Math.random());
+console.log(Math.trunc(Math.random() * 100));
+console.log(Math.sin(90));
+console.log(Math.cos(90));
+console.log(Math.cosh(90));
+console.log(Math.tan(90));
