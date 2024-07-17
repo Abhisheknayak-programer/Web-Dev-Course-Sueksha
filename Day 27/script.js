1557e5cccd3a5568f8c65866db86077eba2eb17b
Abhisheknayak-programer <sahiservice300@gmail.com>
2024-07-17 21:37:16 +0530

    Session 27 Done

---
+++ Day 27/script.js
@@ -0,0 +1,32 @@
+let age = 18;
+
+// // Not True
+// 18 > 18;
+// 18 < 18;
+
+// // True
+// 18 == 18;
+// 18 >= 18;
+// 18 <= 18;
+
+if (age >= 18) {
+  console.log("You are Eligible");
+} else {
+  console.log("You are not Eligible");
+}
+
+let num = 0;
+
+if (num < 0) {
+  console.log("Negative Number");
+} else if (num > 0) {
+  console.log("Positive Number");
+} else {
+  console.log("0");
+}
+
+if (18 == "18") {
+  //   alert("18");
+} else {
+  //   alert("error");
+}
